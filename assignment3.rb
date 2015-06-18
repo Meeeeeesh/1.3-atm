1f14ac10d4581920d0cf475a837b4705127e6540
Michelle Hood <meesh121@gmail.com>
2015-06-18 13:19:54 -0400

    Initial commit

--- assignment3.rb
+++ assignment3.rb
@@ -9,6 +9,7 @@ class Bank
 
   def withdraw(amount)
     puts "What amount would you like to withdraw?"
+    self.available_funds -= amount
     puts "$#{amount} has been successfully withdrawn." 
   end
   
@@ -22,49 +23,77 @@ class Bank
   end
 end
 
-bank = Bank.new("User id")
+bank = Bank.new("existing_user")
 bank.class
 
-puts "Welcome! To make it rain, enter your name:"
+user = User.new({ name:....})
+existing_user = [user1, user2]
 
-cmd = []
-account = Bank.new("user_id")
-user_id = []
-name = user_id
-pin = []
 
-new_name = gets.chomp
-print ">> "
-puts "Gimme dem digits, guh."
-new_pin = gets.chomp
-print ">> "
 
-puts "Select an option from the menu."
-  puts %{
-    1: Check Balance
-    2: Withdraw Funds
-    3: Deposit Funds
-    4: Cancel
-  }
-  puts ">> "
-  cmd = gets.chomp
+selected_option = []
+account = Bank.new("existing_user")
+existing_user = [
+  { name: "Hitler", 
+    pin: "1234",
+    available_funds: 2000 },
+  { name: "Balls", 
+    pin: "1111",
+    available_funds: 2000 }
+]
+
+atm_available_funds = 5000
+
+def prompt_user(text)
+  puts text
+  print ">> "
+  gets.chomp
+end
+
+def prompt_and_chomp(text)
+  input_text = ""
+    while input_text == ""
+      input_text = prompt_user(text)
+    end
+    input_text
+end
+
+
+existing_user = prompt_and_chomp("Welcome! To make it rain, enter your name:")
+current_pin = prompt_and_chomp("Gimme dem digits, guh.")
+
+user_exists = nil
+existing_user.each do |user|
+  if user[:name] == existing_user && user[:pin] == current_pin
+    existing_user = user
+  end
+end
+
+if existing_user
+  options = [ '1: Check Balance', '2: Withdraw Funds', '3: Deposit Funds', '4: Cancel' ]
+  options.each { |o| puts o }
+  selected_option = nil
+  until (1..options.length).include?(selected_option)
+    selected_option = prompt_user(options.join("\n")).to_i
+  end
   
-  case cmd
-    when "1"
+  case selected_option
+    when 1
       puts "Your existing balance is $%.2f\n" % account.balance
-    when "2"
-      puts "What amount would you like to withdraw?"
-      amount = gets.chomp.to_i
-      account.withdraw(amount)
-    when "3"
+    when 2
+      requested_amount = prompt_user("What amount would you like to withdraw?").to_i
+      if requested_amount >= atm_available_funds
+        put "Not enough in machine."
+    when 3
       puts "What amount would you like to deposit?"
       amount = gets.chomp.to_i
       account.deposit(amount)
-    when "4"
+    when 4
       puts "Canceling transaction."
     else
-      puts "Try again." unless cmd == "4"
+      puts "Try again." unless cmd == "5"
     end
+end
 
 # class Bank
 
---
+++ hash.rb
@@ -0,0 +1,10 @@
+hash = {
+  p1: "Ryan",
+  p1a: "29",
+  p2: "Meesh",
+  p2a: "31"
+}
+
+
+puts hash[p1a:]
+puts hash[p2:]
